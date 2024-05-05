61bdda8048ef9a8eaf85ec69cfe21ad68d7573c4
Anderson Siqueira <contato@andersonls.com.br>
2024-05-05 01:16:49 -0300

    uri1067

---
+++ uri1067/uri1067/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+
+            X = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= X; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
