4feb78076bd2a4e5826279ca82496b6eb44f3770
Anderson Siqueira <contato@andersonls.com.br>
2024-05-12 22:19:17 -0300

    ExercicioResolvido01Secao6

---
+++ ExercicioResolvido01Secao6/ExercicioResolvido01Secao6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ENercicioResolvido01Secao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] S = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(S[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}
