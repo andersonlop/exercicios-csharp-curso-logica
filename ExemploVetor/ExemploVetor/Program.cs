aa9b35ef714a86382416b7f54b1ea391c415aa6b
Anderson Siqueira <contato@andersonls.com.br>
2024-05-12 22:20:39 -0300

    Exemplo de Vetor

---
+++ ExemploVetor/ExemploVetor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExemploVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("=====");
+
+            for (int i = 0; i < N; i++)
+            {
+                
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
