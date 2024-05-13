e2e4a3a2506d24188a03885ef22c882b046e076e
Anderson Siqueira <contato@andersonls.com.br>
2024-05-12 22:19:38 -0300

    ExercicioProposto02

---
+++ ExercicioProposto02/ExercicioProposto02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioProposto02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int[] a;
+
+            numero = int.Parse(Console.ReadLine());
+            a = new int[numero];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < numero; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (a[i] % 2 == 0 )
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }
+
+            int soma = 0;
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    soma++;
+                }
+            }
+
+            Console.WriteLine(soma);
+
+            Console.ReadLine();
+
+        }
+    }
+}
